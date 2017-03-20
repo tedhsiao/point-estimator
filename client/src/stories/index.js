29a4a46b5179e892eff7230a0a09c4ac9ea5efc9
Ted Hsiao <abc810221@gmail.com>
2017-03-19 22:47:16 -0700

    Refactor Nav and added NavTab component

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
-import QuestionForm from '../Components/QuestionForm/QuestionForm';
-import Nav from '../Components/Nav/Nav';
-import '@blueprintjs/core/dist/blueprint.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import React from "react";
+import { storiesOf, action, linkTo } from "@kadira/storybook";
+import Button from "./Button";
+import Welcome from "./Welcome";
+import QuestionForm from "../Components/QuestionForm/QuestionForm";
+import Nav from "../Components/Nav/Nav";
+import NavTab from "../Components/NavTab/NavTab";
+import "@blueprintjs/core/dist/blueprint.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
   ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ));
+storiesOf("QuestionForm", module).add("default", () => <QuestionForm />);
 
-storiesOf('QuestionForm', module)
-  .add('default', () => (
-    <QuestionForm />
-  ))
+storiesOf("Nav", module).add("default", () => <Nav />);
 
-storiesOf('Nav', module)
-  .add('default', () => (
-    <Nav />
-  ))
+storiesOf("NavTab", module).add("default", () => (
+  <NavTab
+    buttonText="Home"
+    buttonIcon="pt-icon-home"
+    active=""
+    onClick={() => {
+      console.log("Clicked");
+    }}
+  />
+));
