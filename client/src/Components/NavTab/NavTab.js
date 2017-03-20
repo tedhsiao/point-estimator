f713f6df763c1e6c9c3018bb7cfa67b139cc8dcc
Ted Hsiao <abc810221@gmail.com>
2017-03-20 00:10:36 -0700

    Add NavTab component

---
+++ client/src/Components/NavTab/NavTab.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from "react";
+
+const propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  buttonIcon: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+class NavTab extends Component {
+  constructor() {
+    super();
+  }
+
+  _activeClass() {
+    return this.props.active ? "active-color" : "";
+  }
+
+  render() {
+    return (
+      <button
+        className={
+          `pt-button pt-minimal ${this.props.buttonIcon} ${this._activeClass()}`
+        }
+        onClick={this.props.onClick}
+      >
+        {this.props.buttonText}
+      </button>
+    );
+  }
+}
+
+NavTab.propTypes = propTypes;
+
+export default NavTab;
