b0e475c8a39282791541a87c881facd4411ac4d2
Ted Hsiao <abc810221@gmail.com>
2017-03-23 17:02:40 +0800

    Added Hamburger Menu as Navigation (#24)

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,75 @@ import "./App.css";
 import "@blueprintjs/core/dist/blueprint.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
-import { Menu, MenuItem, Button } from "@blueprintjs/core";
+import { Button } from "@blueprintjs/core";
 import QuestionForm from "./Components/QuestionForm/QuestionForm";
-import Nav from "./Components/Nav/Nav";
+import BurgerMenu from "react-burger-menu";
+import { Link } from "react-router-dom";
+import Radium from "radium";
+
+const Menu = BurgerMenu["slide"];
+
+let MenuWrap = React.createClass({
+  getInitialState() {
+    return { hidden: false };
+  },
+
+  show() {
+    this.setState({ hidden: false });
+  },
+
+  render() {
+    let style;
+
+    if (this.state.hidden) {
+      style = { display: "none" };
+    }
+
+    return (
+      <div style={style} className={this.props.side}>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+let RadiumLink = Radium(Link);
 
 class App extends Component {
+  changeMenu(menu) {
+    this.setState({ currentMenu: menu });
+  }
+  getItem() {
+    let items = [
+      <RadiumLink to="/">Home</RadiumLink>,
+      <RadiumLink to="/room">Room</RadiumLink>,
+      <RadiumLink to="/profile">Profile</RadiumLink>
+    ];
+    return items;
+  }
+  getMenu() {
+    const items = this.getItem();
+
+    return (
+      <MenuWrap wait={20}>
+        <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+          {items}
+        </Menu>
+      </MenuWrap>
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <Nav tabs={["Home", "Room"]} />
-        <Menu className="default pt-dark">
-          <MenuItem text="Vote" />
-          <MenuItem text="My Account" />
-          <MenuItem text="Logout" />
-        </Menu>
-        <Button text="Create a Question" className="pt-intent-primary" />
-        <QuestionForm />
+      <div id="outer-container" style={{ height: "100%" }}>
+        {this.getMenu()}
+        <main id="page-wrap">
+          <div className="App">
+            {this.getMenu()}
+            <Button text="Create a Question" className="pt-intent-primary" />
+            <QuestionForm />
+          </div>
+        </main>
       </div>
     );
   }
