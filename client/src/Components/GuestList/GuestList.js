3616f8d8da6cccba9beb1fb9e062ecd1b935599f
steven <steventsao713@gmail.com>
2017-03-25 22:53:49 -0700

    Add Guestlist and pollchart

---
+++ client/src/Components/GuestList/GuestList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Menu, MenuDivider, MenuItem, Classes } from "@blueprintjs/core";
+
+class GuestList extends React.Component {
+  _renderGuests() {
+    if (this.props.guests.length) {
+      return this.props.guests.map((guest, i) => (
+        <MenuItem
+          key={`${guest.name}-${guest.i}`}
+          text={guest.name}
+          label={guest.choice}
+        />
+      ));
+    } else {
+      return <MenuItem text="No one else has joined yet" />;
+    }
+  }
+
+  render() {
+    return (
+      <Menu>
+        <MenuDivider title="Guests" />
+        {this._renderGuests()}
+      </Menu>
+    );
+  }
+}
+
+GuestList.propTypes = {
+  guests: React.PropTypes.array
+};
+
+export default GuestList;
