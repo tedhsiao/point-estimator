e710d4168ba0cedb3d348a3097b36c436a512bce
Ted Hsiao <abc810221@gmail.com>
2017-04-11 00:51:56 -0700

    rename action names move login button into burger menu add logout button display avialable routes base on user state

--- client/src/Actions/UserActions.js
+++ client/src/Actions/UserActions.js
@@ -1,24 +1,31 @@
 import axios from "axios";
 
-const REQ_DATA = "REQ_DATA";
-const RECV_DATA = "RECV_DATA";
-const RECV_ERROR = "RECV_ERROR";
+export const SIGN_UP_REQ = "SIGN_UP_REQ";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_ERR = "SIGN_UP_ERR";
+export const LOG_OUT = "LOG_OUT";
 
 function requestData() {
-  return { type: REQ_DATA };
+  return { type: SIGN_UP_REQ };
 }
 
 function receiveData(json) {
   return {
-    type: RECV_DATA,
+    type: SIGN_UP_SUCCESS,
     data: json
   };
 }
 
 function receiveError(json) {
   return {
-    type: RECV_ERROR,
+    type: SIGN_UP_SUCCESS,
     data: json
+  };
+}
+
+export function logout() {
+  return {
+    type: LOG_OUT
   };
 }
 
